3faa4b446274d4bf6eb2bffdcd157ce703a301f9
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-05-31 13:07:28 +0300

    Add movie elements

--- IMDBcrawler/pipelines.py
+++ IMDBcrawler/pipelines.py
@@ -18,3 +18,36 @@ class MoviePipeline:
     def process_item(self, item, spider):
         ipdb.set_trace()
         return item
+
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+# useful for handling different item types with a single interface
+
+# from itemadapter import ItemAdapter
+# import json
+# from imdbC.items import Movie, Actor, ActorsAndMovies, ActorFilmography
+# class ImdbcPipeline:
+#     def process_item(self, item, spider):
+#         return item
+# class SimpleStoragePipeline:
+#     def open_spider(self, spider):
+#         self.movie = open("imdb_movie.json", "w")
+#         self.actor = open("imdb_actor.json", "w")
+#         # open 1 file for each type
+#         # connect to db
+#     def process_item(self, item, spider):
+#         # if type of item = Movie insert into movie
+#         if isinstance(item, Movie):
+#             line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+#             self.movie.write(line)
+#             return item
+#         elif isinstance(item, Actor):
+#             line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+#             self.actor.write(line)
+#             return item
+#     def close_spider(self, spider):
+#         self.movie.close()
+#         self.actor.close()
--- IMDBcrawler/spiders/imdb.py
+++ IMDBcrawler/spiders/imdb.py
@@ -1,6 +1,8 @@
 import scrapy
 from IMDBcrawler.items import Movie
 import re
+from datetime import date
+import json
 
 
 class ImdbSpider(scrapy.Spider):
@@ -21,7 +23,32 @@ class ImdbSpider(scrapy.Spider):
         import ipdb
 
         # ipdb.set_trace()
-        arr = re.findall(pattern, response.text)
-        arr = [x.replace('"const":"', "") for x in arr]
-        f = open("test.html", "w")
-        f.write(response.text)
+        arr = re.findall(pattern, txt)
+        arr = [x.replace('"const":"', "https://www.imdb.com/title/") for x in arr]
+        # f = open("test1.txt", "w")
+        # f.write(str(arr))
+        print(arr)
+        print(len(arr))
+
+        for link in arr:
+            yield scrapy.Request(link, callback=self.parse_movie)
+
+    def parse_movie(self, response):
+        movie_items = response.xpath("//*[@id='__next']/main/div/section[1]")
+        for movie in movie_items:
+            movie = Movie()
+
+            movie["category"] = response.xpath(
+                "//div[@class='sc-16ede01-8 hXeKyz sc-910a7330-11 GYbFb']//li[@class='ipc-inline-list__item ipc-chip__text']/text()"
+            ).getall()
+
+            movie["date_of_scraping"] = str(date.today())
+
+            movie["directors"] = response.xpath(
+                "//*[@id='__next']/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[3]/ul/li[1]/div/ul/li/a/text()"
+            ).getall()
+
+            movie["title"] = response.xpath(
+                "//*[@id='__next']/main/div/section[1]/section/div[3]/section/section/div[1]/div[1]/h1/text()"
+            ).getall()
+            print(movie["title"])
